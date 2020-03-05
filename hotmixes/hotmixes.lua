d518267d1ac9ed4a221bd494391edcc769222833
Alexander Chalikiopoulos <Alexander@electricant.com>
2020-03-05 16:46:43 +0100

    find on creation time

--- hotmixes/hotmixes.lua
+++ hotmixes/hotmixes.lua
@@ -36,7 +36,7 @@ function write_hotmixes()
     -- list last 10 modified files in our directory
     function latest_files(directory)
         local i, t, popen = 0, {}, io.popen
-        local pfile = popen('find "'..directory..'" -type f ! -name \'*.filepart\' -printf \'%T@ %p\n\'| sort -n | tail -10 | cut -f2- -d" "| sed s:"'..directory..'/"::')
+        local pfile = popen('find "'..directory..'" -type f ! -name \'*.filepart\' -printf \'%C@ %p\n\'| sort -n | tail -10 | cut -f2- -d" "| sed s:"'..directory..'/"::')
         for filename in pfile:lines() do
             i = i + 1
             t[i] = filename
