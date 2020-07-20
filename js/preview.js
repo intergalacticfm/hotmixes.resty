61315342d0c6d76361aa23e49f9a84e84eabd267
Paul van der Maden <paul@vandermadenconsultancy.nl>
2020-07-20 21:43:16 +0200

    Preview is now started when hovering the icon

--- js/preview.js
+++ js/preview.js
@@ -1,11 +1,6 @@
-function getMixId(e) {
-    let mixId = getMixLink(e).replace(/\./g, '_').replace(/\//g, '_');
+function getMixId(mixlink) {
+    let mixId = mixlink.replace(/\./g, '_').replace(/\//g, '_');
     return mixId;
-}
-
-function getMixLink(e) {
-    let mixLink = e.target.getAttribute("href");
-    return mixLink;
 }
 
 function getSamplesLink(mixLink) {
@@ -15,9 +10,10 @@ function getSamplesLink(mixLink) {
 document.addEventListener('DOMContentLoaded', function () {
     browser = navigator.userAgent.toLowerCase();
 
-    u("a.amixlink").on('mouseenter', function (e) {
-        let mixLink = getMixLink(e);
-        let mixId = getMixId(e);
+    u("img.preview-icon").on('mouseenter', function (e) {
+        let mixLink = u(this).parent('div.mix-holder').find('a.amixlink').attr('href');
+        console.log("mixlink: "+mixLink);
+        let mixId = getMixId(mixLink);
         let samplesLink = getSamplesLink(mixLink);
 
         u("body").append(
@@ -27,8 +23,9 @@ document.addEventListener('DOMContentLoaded', function () {
         )
     });
 
-    u("a.amixlink").on('mouseleave', function (e) {
-        let mixId = getMixId(e);
+    u("img.preview-icon").on('mouseleave', function (e) {
+        let mixLink = u(this).parent('div.mix-holder').find('a.amixlink').attr('href');
+        let mixId = getMixId(mixLink);
         u("#" + mixId).remove();
     });
 });
